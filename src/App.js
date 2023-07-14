7a0759d6ca2f2770823df1a2e7455728dfa2b9b1
sairam-maurya <rams44258@gmail.com>
2023-07-14 02:38:25 -1200

    add friend list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,13 +21,25 @@ const initialFriends = [
   },
 ];
 
+function Button({ children,onClick }) {
+  return <button className="button" onClick={onClick}>{children}</button>
+
+}
+
 export default function App() {
+  const [showAddFriend,setShowAddFriend]=useState(false);
+
+  function handelShowAddFriend(){
+    setShowAddFriend((show)=>!show)
+  }
+  
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend&&
+        <FormAddFriend  />}
+        <Button onClick={handelShowAddFriend}>{showAddFriend? 'Close':'Add Friend'}</Button>
       </div>
       <FormSplitBill />
     </div>
@@ -55,10 +69,7 @@ function Friend({ friend }) {
   </li>
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>
 
-}
 
 function FormAddFriend() {
   return <form className="form-add-friend">
